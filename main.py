f11081be0ccdb491698761da5f79cc7e81994597
SargisVardanian <sargisvardanyan810@gmail.com>
2023-06-25 09:54:34 +0400

    add new file main.py and modify Object_Detection_EfficientNetB0.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import os
 from Train_get import get_train_data, get_validation_data
 
-# Загрузка данных
 train_images, train_labels = get_train_data()
 validation_images, validation_labels = get_validation_data()
 
-# Остальной код из файла Object_Detection_EfficientNetB0.py
-
-# Создание модели, обучение и сохранение
 
 script_path = 'Object_Detection_EfficientNetB0.py'
 n = 5
